44f0ccdd12cae466b4202eea72496e9d55e185ba
yanfeiwuji <1327800522@qq.com>
2024-05-16 17:04:56 +0800

    easy to handler logic

--- src/main/java/io/github/yanfeiwuji/isupabase/request/filter/MLogicalOperators.java
+++ src/main/java/io/github/yanfeiwuji/isupabase/request/filter/MLogicalOperators.java
@@ -4,19 +4,26 @@ import com.mybatisflex.core.query.QueryCondition;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 
 @UtilityClass
 @Slf4j
 public class MLogicalOperators {
     public final Operator AND = new Operator("and", Pattern.compile("^and(.*)"),
-            f -> QueryCondition.createEmpty().and(f.getFilters().stream().map(Filter::toQueryCondition)
-                    .reduce(QueryCondition.createEmpty(), QueryCondition::and))
+            MLogicalOperators.apply(QueryCondition::and)
 
     );
     public final Operator OR = new Operator("or", Pattern.compile("^or(.*)"),
-            f -> QueryCondition.createEmpty().and(f.getFilters().stream().map(Filter::toQueryCondition)
-                    .reduce(QueryCondition::or)
-                    .orElse(QueryCondition.createEmpty()))
+            MLogicalOperators.apply(QueryCondition::or)
     );
+
+    public Function<Filter, QueryCondition> apply(BinaryOperator<QueryCondition> binaryOperator) {
+        return f -> QueryCondition.createEmpty()
+
+                .and(f.getFilters().stream().map(Filter::toQueryCondition)
+                .reduce(binaryOperator)
+                .orElse(QueryCondition.createEmpty()));
+    }
 }
