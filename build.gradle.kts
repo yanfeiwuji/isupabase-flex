11abcec25a33b7b2bdc3c0b3ebb16a9b900b9343
烟霏雾集 <1327800522@qq.com>
2024-06-30 20:49:18 +0800

    clear

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     `maven-publish`
     id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
-    // id("org.graalvm.buildtools.native") version "0.10.2"
 }
 
 group = "io.github.yanfeiwuji"
